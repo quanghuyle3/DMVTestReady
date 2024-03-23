32ce2a080c36171b1cd1e7cea6d7ef1492b9bfca
Quang Le <quanghuyle3@gmail.com>
2024-03-23 16:16:22 -0700

    Added routes and more templates

--- app.py
+++ app.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def practice():
+    return render_template("practice.html")
+
+@app.route('/index')
 def index():
     return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/registration')
+def registration():
+    return render_template("registration.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
