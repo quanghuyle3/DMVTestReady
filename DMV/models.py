da2ddc54eb53c5aed5b49f9c69ddd88481234d8c
Quang Le <quanghuyle3@gmail.com>
2024-04-14 01:26:01 -0700

    Added models for score history

--- DMV/models.py
+++ DMV/models.py
@@ -1,6 +1,8 @@
 from . import db 
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 
 class User(db.Model, UserMixin): #UserMixin is specific for USER
@@ -8,13 +10,29 @@ class User(db.Model, UserMixin): #UserMixin is specific for USER
     email = db.Column(db.String(150), unique = True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    
+
+class Practice_scores(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = relationship('User', backref='practice_scores')
+    practice_name = db.Column(db.String(150))
+    score = db.Column(db.Integer) 
+    timestamp = db.Column(db.DateTime, default=datetime.now)
+
+class Exam_scores(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = relationship('User', backref='exam_scores')
+    exam_name = db.Column(db.String(150))
+    score = db.Column(db.Integer) 
+    timestamp = db.Column(db.DateTime, default=datetime.now)
     
 
 # Model for question
 class Question:
-    def __init__(self, id, question, a, b, c, d, answer, chose=''):
+    def __init__(self, id, type, question, a, b, c, d, answer, chose=''):
         self.id = id
+        self.type = type
         self.question = question 
         self.a = a
         self.b = b 
